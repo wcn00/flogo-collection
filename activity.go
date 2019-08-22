bcebff81d0ea11062f6f61d801ba4f8df332e7dc
Wendell Nichols <wnichols@tibco.com>
2019-08-22 08:20:56 -0600

    Rough in model

--- activity.go
+++ activity.go
@@ -1,26 +1,44 @@
 package collection
 
 import (
+	"sync"
+
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/util"
 )
 
-// MyActivity is a stub for your Activity implementation
-type MyActivity struct {
-	metadata *activity.Metadata
+var collectionCacheMutex sync.Mutex
+
+// Collection static structure containing all aggregations.
+type Collection struct {
+	metadata  *activity.Metadata
+	colmap    map[string][]interface{}
+	generator *util.Generator
+}
+
+var col *Collection
+
+func init() {
+	col = new(Collection)
+	col.colmap = make(map[string][]interface{})
+	gen, err := util.NewGenerator()
+	if err == nil {
+		col.generator = gen
+	}
 }
 
 // NewActivity creates a new activity
 func NewActivity(metadata *activity.Metadata) activity.Activity {
-	return &MyActivity{metadata: metadata}
+	return &Collection{metadata: metadata}
 }
 
 // Metadata implements activity.Activity.Metadata
-func (a *MyActivity) Metadata() *activity.Metadata {
+func (a *Collection) Metadata() *activity.Metadata {
 	return a.metadata
 }
 
 // Eval implements activity.Activity.Eval
-func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+func (a *Collection) Eval(context activity.Context) (done bool, err error) {
 
 	// do eval
 
